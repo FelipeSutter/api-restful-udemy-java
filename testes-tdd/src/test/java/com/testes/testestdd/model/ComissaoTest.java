0345ce0e64da58c852540a5aa7da9f46a67de47b
Felipe Sutter <lipeps61@gmail.com>
2023-12-29 18:44:50 -0300

    feat: adicionado configuração global para os testes

--- testes-tdd/src/test/java/com/testes/testestdd/model/ComissaoTest.java
+++ testes-tdd/src/test/java/com/testes/testestdd/model/ComissaoTest.java
@@ -2,18 +2,34 @@ package com.testes.testestdd.model;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 
 // Colocar essa anotação para dizer que é uma classe de teste
 @SpringBootTest
 public class ComissaoTest {
+
+    // Config global que será executada antes dos outros testes
+    @TestConfiguration
+    static class ComissaoConfig {
+
+        @Bean
+        public Comissao comissao() {
+            return new Comissao();
+        }
+
+    }
+
+    @Autowired
+    Comissao comissao;
 
     // Para cada caso de teste é criado um método e também se coloca uma anotação
     // indicando que é um teste
     @Test
     public void deve_calcular_100_reais_de_comissao_para_venda_de_1000_com_10_por_cento() {
         // Arrange
-        Comissao comissao = new Comissao();
         Double valorVenda = 1000.0;
         Double valorComissao = 100.0;
 
@@ -28,7 +44,6 @@ public class ComissaoTest {
     @Test
     public void deve_calcular_300_reais_de_comissao_para_venda_de_2000_com_15_por_cento() {
         // Arrange
-        Comissao comissao = new Comissao();
         Double valorVenda = 2000.0;
         Double valorComissao = 300.0;
 
