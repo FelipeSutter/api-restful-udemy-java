175d2f4304384e568e8b6a4eadbe40b157b02590
Felipe Sutter <lipeps61@gmail.com>
2023-12-30 21:16:20 -0300

    feat: controller de produto feito

--- testes-tdd/src/test/java/com/testes/testestdd/controller/ProdutoControllerTest.java
+++ testes-tdd/src/test/java/com/testes/testestdd/controller/ProdutoControllerTest.java
@@ -1,5 +1,12 @@
 package com.testes.testestdd.controller;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest
 public class ProdutoControllerTest {
+
+    // MockMvc
+    // Mockito
+    // Mock
 
 }
