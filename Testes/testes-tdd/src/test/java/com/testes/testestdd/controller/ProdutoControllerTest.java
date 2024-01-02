5d6190c2e449c667dab198e83d4de8758ba4cf4e
Felipe Sutter <lipeps61@gmail.com>
2024-01-02 16:14:31 -0300

    feat: criado o primeiro test controller com mockMvc

--- Testes/testes-tdd/src/test/java/com/testes/testestdd/controller/ProdutoControllerTest.java
+++ Testes/testes-tdd/src/test/java/com/testes/testestdd/controller/ProdutoControllerTest.java
@@ -1,6 +1,18 @@
 package com.testes.testestdd.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.testes.testestdd.model.Produto;
 
 @WebMvcTest
 public class ProdutoControllerTest {
@@ -8,5 +20,31 @@ public class ProdutoControllerTest {
     // MockMvc
     // Mockito
     // Mock
+
+    @Autowired
+    private ProdutoController controller;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        // Será executado antes de qualquer caso de teste por conta da anotação.
+        // Esse cara vai simular uma requisição do controller instanciado acima
+        this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void deve_retornar_status_200_OK_ao_chamar_obter_todos() throws Exception {
+        // Arrange
+        // List<Produto> produtos = new ArrayList<Produto>();
+        var requestBuilder = MockMvcRequestBuilders.get("/api/produtos"); // criando a requisição
+
+        // Act
+        this.mockMvc.perform(requestBuilder).andExpect(MockMvcResultMatchers.status().isOk()); // esse método
+                                                                                               // envia a
+        // requisição
+        // Assert
+    }
 
 }
