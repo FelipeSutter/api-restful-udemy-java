3fd762e6275f14bc736930b5b8ea9604f3782fc2
Felipe Sutter <lipeps61@gmail.com>
2024-01-04 17:06:56 -0300

    feat: added the controller for usuario

--- APIRestful/gestao-de-projetos/src/main/java/com/api/gestaodeprojetos/view/controller/UsuarioController.java
+++ APIRestful/gestao-de-projetos/src/main/java/com/api/gestaodeprojetos/view/controller/UsuarioController.java
@@ -1,12 +1,50 @@
 package com.api.gestaodeprojetos.view.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.api.gestaodeprojetos.model.Usuario;
+import com.api.gestaodeprojetos.service.UsuarioService;
+import com.api.gestaodeprojetos.view.model.LoginRequest;
+import com.api.gestaodeprojetos.view.model.LoginResponse;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/api/usuarios")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioService service;
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> obterTodos() {
+        return new ResponseEntity<>(service.obterTodos(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Usuario>> obterPorId(@PathVariable Long id) {
+        return new ResponseEntity<>(service.obterPorId(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> adicionar(@RequestBody Usuario usuario) {
+        return new ResponseEntity<>(service.adicionar(usuario), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        return new ResponseEntity<>(service.logar(request.getEmail(), request.getSenha()), HttpStatus.CREATED);
+    }
 
 }
