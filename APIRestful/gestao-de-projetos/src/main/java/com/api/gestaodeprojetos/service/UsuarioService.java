cbcf8d4d29edbccb36cfc04c2d012c45f8f92d76
Felipe Sutter <lipeps61@gmail.com>
2024-01-04 16:14:50 -0300

    feat: login method complete

--- APIRestful/gestao-de-projetos/src/main/java/com/api/gestaodeprojetos/service/UsuarioService.java
+++ APIRestful/gestao-de-projetos/src/main/java/com/api/gestaodeprojetos/service/UsuarioService.java
@@ -1,24 +1,39 @@
 package com.api.gestaodeprojetos.service;
 
+import java.util.Collections;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.api.gestaodeprojetos.model.Usuario;
 import com.api.gestaodeprojetos.repository.UsuarioRepository;
+import com.api.gestaodeprojetos.security.JWTService;
+import com.api.gestaodeprojetos.view.model.LoginResponse;
 
 @Service
 public class UsuarioService {
+
+    private static final String headerPrefix = "Bearer ";
 
     @Autowired
     private UsuarioRepository repository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JWTService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     public List<Usuario> obterTodos() {
         return repository.findAll();
@@ -43,6 +58,21 @@ public class UsuarioService {
         usuario.setSenha(senhaCriptografada);
 
         return repository.save(usuario);
+
+    }
+
+    public LoginResponse logar(String email, String senha) {
+
+        Authentication auth = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(email, senha, Collections.emptyList()));
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        String token = headerPrefix + jwtService.gerarToken(auth);
+
+        Usuario usuario = repository.findByEmail(email).get();
+
+        return new LoginResponse(token, usuario);
 
     }
 
