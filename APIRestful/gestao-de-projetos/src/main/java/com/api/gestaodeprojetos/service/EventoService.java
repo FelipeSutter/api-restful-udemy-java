d250a7e712aa0457f950e9bac08aac6a17839eda
Felipe Sutter <lipeps61@gmail.com>
2024-01-08 14:51:30 -0300

    feat: added implementations in service and controller for Evento

--- APIRestful/gestao-de-projetos/src/main/java/com/api/gestaodeprojetos/service/EventoService.java
+++ APIRestful/gestao-de-projetos/src/main/java/com/api/gestaodeprojetos/service/EventoService.java
@@ -1,8 +1,12 @@
 package com.api.gestaodeprojetos.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.api.gestaodeprojetos.model.Evento;
 import com.api.gestaodeprojetos.repository.EventoRepository;
 
 @Service
@@ -10,5 +14,22 @@ public class EventoService {
 
     @Autowired
     private EventoRepository repository;
+
+    public List<Evento> obterTodos() {
+        return repository.findAll();
+    }
+
+    public Optional<Evento> obterPorId(Long id) {
+        return repository.findById(id);
+    }
+
+    public Evento adicionar(Evento evento) {
+        return repository.save(evento);
+    }
+
+    public Evento atualizar(Long id, Evento evento) {
+        // Evento.setId(id);
+        return repository.save(evento);
+    }
 
 }
--- APIRestful/gestao-de-projetos/src/main/java/com/api/gestaodeprojetos/view/controller/EventoController.java
+++ APIRestful/gestao-de-projetos/src/main/java/com/api/gestaodeprojetos/view/controller/EventoController.java
@@ -1,9 +1,18 @@
 package com.api.gestaodeprojetos.view.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.gestaodeprojetos.model.Evento;
 import com.api.gestaodeprojetos.service.EventoService;
 
 @RestController
@@ -12,5 +21,26 @@ public class EventoController {
 
     @Autowired
     private EventoService service;
+
+    @GetMapping
+    public List<Evento> obterTodos() {
+        return service.obterTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Evento> obterPorId(@PathVariable Long id) {
+        return service.obterPorId(id);
+    }
+
+    @PostMapping
+    public Evento adicionar(Evento evento) {
+        return service.adicionar(evento);
+    }
+
+    @PutMapping
+    public Evento atualizar(@PathVariable Long id, @RequestBody Evento evento) {
+        // evento.setId(id);
+        return service.atualizar(id, evento);
+    }
 
 }
