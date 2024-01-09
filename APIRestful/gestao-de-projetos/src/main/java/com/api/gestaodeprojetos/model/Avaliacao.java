cfdecc163e02994d3d87f7251bd86f2df5965951
Felipe Sutter <lipeps61@gmail.com>
2024-01-09 16:48:41 -0300

    feat: added implementation in model for avaliacao and pessoa

--- APIRestful/gestao-de-projetos/src/main/java/com/api/gestaodeprojetos/model/Avaliacao.java
+++ APIRestful/gestao-de-projetos/src/main/java/com/api/gestaodeprojetos/model/Avaliacao.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Avaliacao {
@@ -14,12 +16,34 @@ public class Avaliacao {
     @Column(name = "avaliacao_id")
     private Long id;
 
+    private String descricao;
+
+    @OneToOne
+    @JoinColumn(name = "pessoa_id")
+    private Pessoa pessoa;
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
     }
 
 }
--- APIRestful/gestao-de-projetos/src/main/java/com/api/gestaodeprojetos/model/Pessoa.java
+++ APIRestful/gestao-de-projetos/src/main/java/com/api/gestaodeprojetos/model/Pessoa.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Pessoa {
@@ -14,12 +15,33 @@ public class Pessoa {
     @Column(name = "pessoa_id")
     private Long id;
 
+    private String nome;
+
+    @OneToOne(mappedBy = "pessoa")
+    private Avaliacao avaliacao;
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Avaliacao getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Avaliacao avaliacao) {
+        this.avaliacao = avaliacao;
     }
 
 }
